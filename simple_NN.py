f9fe98ad851a4318095ecd6de844eed740602e0c
FatFrederik <dorhasenbroek@gmail.com>
2024-03-07 18:50:01 +0100

    add stopping criteria

--- simple_NN.py
+++ simple_NN.py
@@ -1,6 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import Adam
+from keras.utils.vis_utils import plot_model
+from keras.callbacks import EarlyStopping
 import pandas as pd
 import matplotlib
 matplotlib.use('TkAgg')
@@ -9,13 +11,16 @@ import matplotlib.pyplot as plt
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE" #Prevents a plotting error not sure why
 
-def create_model(input_shape = (600,)):
+def create_model(input_shape, plot = False):
     model = Sequential()
     model.add(Dense(128, input_shape=input_shape))  # first dense layer, 32 hidden units
     model.add(Activation('relu'))  # activation layer
     model.add(Dense(3))  # second dense layer
     model.add(Activation('softmax'))  # output class probabilities
     model.summary()
+
+    if plot == True:
+        plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
     return model
 
 def compile_model(model, learning_rate = 0.001):
@@ -24,7 +29,8 @@ def compile_model(model, learning_rate = 0.001):
     return model
 
 def train_model(model, plot = False):
-    history = model.fit(x_train, y_train, epochs=25, batch_size=32, validation_split=1 / 6)
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3) # stopping criteria
+    history = model.fit(x_train, y_train, callbacks=es, epochs=25, batch_size=32, validation_split=1 / 6)
 
     if plot == True:
         # summarize history for accuracy
@@ -45,10 +51,8 @@ def train_model(model, plot = False):
         plt.show()
 
 
-
-
 x_train = pd.read_csv("X_train.csv")
 y_train = pd.read_csv("y_train.csv")
-model = create_model()
+model = create_model((600,),True)
 model = compile_model(model)
 train_model(model,True)
